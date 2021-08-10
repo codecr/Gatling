7e8f3eaf500eb560810f22599320594da8f38caf
Gerardo Arroyo <gerardo@flecharoja.com>
2021-08-10 07:58:26 -0600

    first commit

---
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011-2018 GatlingCorp (https://gatling.io)
+ *
+ * All rights reserved.
+ */
+
+package frontline.sample
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http
+    .baseUrl("https://inclusionfinanciera.gob.sv/")
+
+  val scn = scenario("scenario1")
+    .exec(
+      http("Home")
+        .get("/")
+    )
+    .exec(
+      http("CNIEF")
+        .get("/cnief")
+    )
+    .exec(
+      http("Inclusion")
+        .get("/inclusion-financiera")
+    )	
+    .exec(
+      http("Educacion Financiera")
+        .get("/educacion-financiera")
+    )
+	.exec(
+      http("Bitcoin")
+        .get("/bitcoin-en-el-salvador")
+    )
+    .exec(
+      http("Reportes Anuales")
+        .get("/reportes-anuales")
+    )	
+    .exec(
+      http("Publicaciones")
+        .get("/publicaciones")
+    )
+    .exec(
+      http("Contacto")
+        .get("/contacto")
+    )	
+  setUp(
+    scn.inject(rampUsers(100).during(300.seconds))
+  ).protocols(httpConf)
+}
